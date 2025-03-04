90589c8e562e001eba7d02d2139a7e5c7833a700
WhatEverKid07 <eugeniosanc22@outlook.com>
2025-03-04 14:38:30 +0000

    Added new pistol model

--- Assets/Gun/Grenade.cs
+++ Assets/Gun/Grenade.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    [SerializeField] float grenadeFuse = 5;
+    [SerializeField] private float grenadeFuse = 3;
+    [SerializeField] private float radius = 5f;
+    [SerializeField] private float grenadeForce = 700f;
+    [SerializeField] private GameObject explosionEffect;
     public void StartFuse()
     {
         StartCoroutine(Fuse(grenadeFuse));
@@ -13,6 +16,24 @@ public class Grenade : MonoBehaviour
     {
         Debug.Log("Works");
         yield return new WaitForSeconds(fuseLength);
+        Explode();
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        /*
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(grenadeForce, transform.position, radius);
+            }
+        }
+        */
         Destroy(gameObject);
     }
 }
