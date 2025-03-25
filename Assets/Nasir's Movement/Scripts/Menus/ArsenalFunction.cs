82cde4ef5cbe9fbe3a9a679e1bb43c82ec52eb13
RisanStar <nBuckley2023@elam.co.uk>
2025-03-25 14:42:47 +0000

    Select function start

--- Assets/Nasir's Movement/Scripts/Menus/ArsenalFunction.cs
+++ Assets/Nasir's Movement/Scripts/Menus/ArsenalFunction.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ArsenalFunction : MonoBehaviour
 {
     [Header("Scripts")]
@@ -12,6 +12,7 @@ public class ArsenalFunction : MonoBehaviour
     private GameObject currentGun;
     private GameObject previousGun;
     private GameObject nextGun;
+    private GameObject selectedGun;
     [SerializeField] private Transform currentOrientation;
 
     private bool canSwitchUp = false;
@@ -37,14 +38,17 @@ public class ArsenalFunction : MonoBehaviour
     {
         currentIndex = System.Array.IndexOf(allGuns, currentGun);
 
-        Debug.Log("Is switching: " + isSwitching);
-        Debug.Log("Can switch up: " + canSwitchUp);
-        Debug.Log("Can switch down: " + canSwitchDown);
+        //Debug.Log("Is switching: " + isSwitching);
+        //Debug.Log("Can switch up: " + canSwitchUp);
+        //Debug.Log("Can switch down: " + canSwitchDown);
 
-        Debug.Log("The current index is: " +  currentIndex);
-        Debug.Log("Previous gun: " + previousGun);
-        Debug.Log("Current gun: " + currentGun);
-        Debug.Log("Next gun: " + nextGun);
+        //Debug.Log("The current index is: " +  currentIndex);
+        //Debug.Log("Previous gun: " + previousGun);
+        //Debug.Log("Current gun: " + currentGun);
+        //Debug.Log("Next gun: " + nextGun);
+        //Debug.Log("Selected gun: " + selectedGun);
+
+        Debug.Log("GoToStory: " + mainMenuScript.goToStory);
 
         if (isSwitching)
         {
@@ -78,6 +82,20 @@ public class ArsenalFunction : MonoBehaviour
         if (currentIndex > -1)
         {
             canSwitchDown = true;
+        }
+    }
+
+    public void SelectWeapon()
+    {
+        selectedGun = currentGun;
+
+        if (mainMenuScript.goToStory)
+        {
+            SceneManager.LoadScene("MovementTest", LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene("EndlessMode", LoadSceneMode.Single);
         }
     }
 
@@ -127,15 +145,17 @@ public class ArsenalFunction : MonoBehaviour
         float rotationTime = 1f;
         float elapsedTime = 0f;
 
-        Vector3 moveLeft = new(currentOrientation.position.x + 15f, currentOrientation.position.y, currentOrientation.position.z);
+        Vector3 currentLeft = new(currentOrientation.position.x + 15f, 0, currentOrientation.position.z);
+        Vector3 nextLeft = new(currentOrientation.position.x - 15f, 0, currentOrientation.position.z);
+
 
         while (elapsedTime < rotationTime)
         {
             currentGun.transform.position = Vector3.Lerp(currentGun.transform.position, currentOrientation.position, switchSpeed * Time.deltaTime);
-            previousGun.transform.position = Vector3.Lerp(previousGun.transform.position, moveLeft, switchSpeed * Time.deltaTime);
+            previousGun.transform.position = Vector3.Lerp(previousGun.transform.position, currentLeft, switchSpeed * Time.deltaTime);
             if (nextGun != null)
             {
-                nextGun.transform.position = Vector3.Lerp(nextGun.transform.position, moveLeft, switchSpeed * Time.deltaTime);
+                nextGun.transform.position = Vector3.Lerp(nextGun.transform.position, nextLeft, switchSpeed * Time.deltaTime);
             }
 
             elapsedTime += Time.deltaTime;
@@ -152,7 +172,8 @@ public class ArsenalFunction : MonoBehaviour
         float rotationTime = 1f;
         float elapsedTime = 0f;
 
-        Vector3 moveRight = new(currentOrientation.position.x - 15f, currentOrientation.position.y, currentOrientation.position.z);
+        Vector3 currentRight = new(currentOrientation.position.x - 15f, 0, currentOrientation.position.z);
+        Vector3 nextRight = new(currentOrientation.position.x + 15f, 0, currentOrientation.position.z);
 
         while (elapsedTime < rotationTime)
         {
@@ -160,12 +181,12 @@ public class ArsenalFunction : MonoBehaviour
 
             if(previousGun != null)
             {
-                previousGun.transform.position = Vector3.Lerp(previousGun.transform.position, moveRight, switchSpeed * Time.deltaTime);
+                previousGun.transform.position = Vector3.Lerp(previousGun.transform.position, currentRight, switchSpeed * Time.deltaTime);
             }
 
             if (nextGun != null)
             {
-                nextGun.transform.position = Vector3.Lerp(nextGun.transform.position, moveRight, switchSpeed * Time.deltaTime);
+                nextGun.transform.position = Vector3.Lerp(nextGun.transform.position, nextRight, switchSpeed * Time.deltaTime);
             }
 
             elapsedTime += Time.deltaTime;
