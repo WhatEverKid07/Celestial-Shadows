d35002425d80011850fe6fa5b2e783c0617a97a2
WhatEverKid07 <eugeniosanc22@outlook.com>
2025-03-25 21:23:11 +0000

    Update
    When inspecting the knife you cant attack, and vice versa, this smooths out the animations and is just basically better.

--- Assets/ECL/Scripts/KnifeAnimation.cs
+++ Assets/ECL/Scripts/KnifeAnimation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,19 +8,20 @@ public class KnifeAnimation : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private InputActionAsset controls;
-
     [SerializeField] private AnimationClip[] attackAnims;
 
     private InputAction inspect;
     private InputAction attack;
 
-    private float timeThreshold = 0.2f; // Time window for rapid presses
+    private float timeThreshold = 0.2f;
     private float lastPressTime = -1f;
     private bool isRapidPressing = false;
     private bool isSpinning = false;
     private bool animCanPlay = true;
+    private bool isAttacking = false;
+    private bool isInspecting = false;
 
-    public bool IsRapidPressing => isRapidPressing; // Public getter for the bool
+    public bool IsRapidPressing => isRapidPressing;
 
     private void Awake()
     {
@@ -34,27 +36,6 @@ public class KnifeAnimation : MonoBehaviour
         inspect.canceled += OnButtonReleased;
         attack.performed += _ => StartAttackAnimation(); 
     }
-    private void StartAttackAnimation()
-    {
-        if (animCanPlay && gameObject.activeInHierarchy)
-        {
-            StartCoroutine(SelectAttackAnim());
-        }
-    }
-    private IEnumerator SelectAttackAnim()
-    {
-        if (!animCanPlay || attackAnims.Length == 0 || animator == null) yield break;
-
-        animCanPlay = false;
-        int randomIndex = Random.Range(0, attackAnims.Length);
-        string selectedAnimation = attackAnims[randomIndex].name;
-        float waitTime = attackAnims[randomIndex].length;
-
-        animator.Play(selectedAnimation);
-        yield return new WaitForSeconds(waitTime);
-        animCanPlay = true;
-    }
-
     private void OnDisable()
     {
         inspect.Disable();
@@ -65,17 +46,35 @@ public class KnifeAnimation : MonoBehaviour
         inspect.Enable();
         attack.Enable();
     }
+    private void StartAttackAnimation()
+    {
+        if (animCanPlay && gameObject.activeInHierarchy)
+        {
+            StartCoroutine(SelectAttackAnim());
+        }
+    }
+    private IEnumerator SelectAttackAnim()
+    {
+        if (!animCanPlay || attackAnims.Length == 0 || animator == null && isInspecting) yield break;
 
+        animCanPlay = false;
+        int randomIndex = Random.Range(0, attackAnims.Length);
+        string selectedAnimation = attackAnims[randomIndex].name;
+        float waitTime = attackAnims[randomIndex].length;
+        animator.Play(selectedAnimation);
+        isAttacking = true;
+        yield return new WaitForSeconds(waitTime);
+        animCanPlay = true;
+        isAttacking = false;
+    }
     private void OnButtonPressed(InputAction.CallbackContext context)
     {
-        if (!isSpinning)
+        if (!isSpinning && !isAttacking)
         {
             animator.SetTrigger("StartSpin");
         }
         float currentTime = Time.time;
-
-        // If the press is within the allowed timeframe, set bool to true
-        if (lastPressTime > 0 && (currentTime - lastPressTime) <= timeThreshold)
+        if (lastPressTime > 0 && (currentTime - lastPressTime) <= timeThreshold && !isAttacking)
         {
             isRapidPressing = true;
             animator.SetBool("LoopSpin?", true);
@@ -84,24 +83,26 @@ public class KnifeAnimation : MonoBehaviour
         lastPressTime = currentTime;
         isSpinning = true;
     }
-
     private void OnButtonReleased(InputAction.CallbackContext context)
     {
-        // Start a coroutine to check if no presses happen within the threshold
         StartCoroutine(CheckForStopPressing());
     }
-
     private IEnumerator CheckForStopPressing()
     {
         float stopTime = lastPressTime;
         yield return new WaitForSeconds(timeThreshold);
 
-        // If no new press happened in the timeframe, reset the bool
         if (lastPressTime == stopTime)
         {
             isRapidPressing = false;
             animator.SetBool("LoopSpin?", false);
             isSpinning = false;
         }
+    }
+    private void StartOfInspectAnim()
+    {
+        isInspecting = !isInspecting;
+        animCanPlay = !animCanPlay;
+        //Debug.LogWarning(isInspecting);
     }
 }
