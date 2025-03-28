fa4cc410c3c0838ef414758ea5feaa42a249bd96
RisanStar <nBuckley2023@elam.co.uk>
2025-03-28 15:41:04 +0000

    Watch

--- Assets/Nasir's Movement/Scripts/Player/ItemScripts/WatchFunction.cs
+++ Assets/Nasir's Movement/Scripts/Player/ItemScripts/WatchFunction.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WatchFunction : MonoBehaviour
@@ -7,13 +5,40 @@ public class WatchFunction : MonoBehaviour
     [Header("Scripts")]
     [SerializeField] private PlayerStats playerStatsScript;
 
+    [Header("Watch")]
+    private GameObject clock;
+    private GameObject chain;
+    private GameObject time;
+
     internal bool canUpdateWatchStat;
+
     private void Start()
     {
-        playerStatsScript.watches.Add(gameObject);
-        canUpdateWatchStat = true;
+        playerStatsScript = FindAnyObjectByType<PlayerStats>();
 
-        Invoke(nameof(NoMoreUpdates), .01f);
+        clock = GameObject.Find("clock");
+        chain = GameObject.Find("chain");
+        time = GameObject.Find("time");
+    }
+
+    private void Update()
+    {
+        //transform.Rotate(transform.position, 180);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == GameObject.Find("PlayerPhys"))
+        {
+            playerStatsScript.watches.Add(gameObject);
+            canUpdateWatchStat = true;
+
+            clock.SetActive(false);
+            chain.SetActive(false);
+            time.SetActive(false);
+
+            Invoke(nameof(NoMoreUpdates), .001f);
+        }
     }
 
     private void NoMoreUpdates()
