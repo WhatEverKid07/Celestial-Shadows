99936bcd263459ab95dc83bd8a674c97d41da6a3
WhatEverKid07 <eugeniosanc22@outlook.com>
2025-05-12 10:41:44 +0100

    Added, cutscene scene

---
+++ Assets/Cutscene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer cutscene;
+    void Start()
+    {
+        StartCoroutine(CutsceneEnd());
+    }
+
+    private IEnumerator CutsceneEnd()
+    {
+        yield return new WaitForSeconds((float)cutscene.clip.length);
+        SceneManager.LoadScene("TEST");
+    }
+}
